355b30d70af12a3dc1dd81d56ee2202dab61ba17
Juan Pablo Garcia <45505790+JPGarCar@users.noreply.github.com>
2021-01-27 15:16:45 -0800

    Added the student class

---
+++ classes/student.js
@@ -0,0 +1,63 @@
+const { GuildMember } = require("discord.js");
+
+/**
+ * Represents a UBC student, being part of the eSports league.
+ */
+module.exports = class Student {
+    
+    /**
+     * Creates a new student object.
+     * @param {String} name - the name of the student
+     * @param {GuildMember} guildMember - the guild member the student is
+     */
+    constructor(name, guildMember) {
+        /**
+         * The student's name.
+         * @type {String}
+         */
+        this.name = name;
+
+        /**
+         * The Discord Guild Member this student uses.
+         * @type {GuildMember}
+         */
+        this.member = guildMember;
+
+        /**
+         * All the leagues this student is part of.
+         * @type {[TODO]}
+         */
+        this.leagues = [];
+
+        /**
+         * The fun points this students holds.
+         * @type {Number}
+         */
+        this.funPoints = 0;
+    }
+
+    /**
+     * Adds fun points to this user.
+     * @param {Number} points - the points to add
+     */
+    addFunPoints(points) {
+        this.funPoints += points;
+    }
+
+    /**
+     * Removes fun points from this user.
+     * @param {Number} points - the points to remove
+     */
+    removeFunPoints(points) {
+        this.funPoints -= points;
+    }
+
+    /**
+     * Adds a league to the user.
+     * @param {TODO} league 
+     */
+    joinLeague(league) {
+        this.leagues.push(league);
+    }
+
+}
