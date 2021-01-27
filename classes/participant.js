5c8f6c46e372e19acd68768688c0e57027c9db7d
Juan Pablo Garcia <45505790+JPGarCar@users.noreply.github.com>
2021-01-27 15:23:59 -0800

    Created the participant class without functions

---
+++ classes/participant.js
@@ -0,0 +1,38 @@
+const { Collection } = require("discord.js");
+const Student = require("./student");
+
+/**
+ * Represents a league player, it must be part of a student. A student can be multiple participants.
+ */
+module.exports = class Participant {
+
+    
+    constructor(league, student) {
+
+        /**
+         * The league this participant is a part of.
+         * @type {TODO}
+         */
+        this.league = league;
+
+        /**
+         * The student represented by this Participant.
+         * @type {Student}
+         */
+        this.student = student;
+
+        /**
+         * The points the participants has.
+         * @type {Number}
+         */
+        this.points = 0;
+
+        /**
+         * The games this participant is scheduled for or played.
+         * @type {Collection<String, TODO>} - <Game ID, Game>
+         */
+        this.games = new Collection();
+
+    }
+
+}
